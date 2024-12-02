ab2374748c3cdd225821c65f82773e9f6a9c0793
24LP7490 24LP7490 <itsmebuya@gmail.com>
2024-12-02 17:15:15 +0800

    variable

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hi")
+
+// 1. "Comments can make code readable"
+
+const firstName = "";
+const lastName = "";
+let maritalStatus;
+let country;
+let age;
+
+let fname, lname, mstatus, myCounty, myAge;
